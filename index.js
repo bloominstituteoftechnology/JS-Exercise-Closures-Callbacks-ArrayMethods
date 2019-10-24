55dba064fb0eab622eb974c94a9e6eec5ed20ae8
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-24 15:21:58 +0200

    example

--- index.js
+++ index.js
@@ -1,37 +1,226 @@
-/*
+// ⭐️ Example Challenge START ⭐️
+
+/**
+ * ### Challenge `firstItem`
+ * 
+ * @instructions
+ * Implement a higher-order function called `firstItem`.
+ * It takes two arguments:
+ * @param {string[]} stringList an array of strings.
+ * @param {Function} callback that takes a single string as its argument.
+ * It returns:
+ * @returns {*} the result of invoking the callback with the first element in the array.
+ * 
+ * Example of usage of this higher-order function:
+ * Invoking `firstItem` passing `['foo', 'bar']` and `(str) => str + str`,
+ * should return 'foofoo'.
+*/
+function firstItem(stringList, callback) {
+  return callback(stringList[0])
+}
+
+// ⭐️ Example Challenge END ⭐️
+
 
 // 👇 COMPLETE YOUR WORK BELOW 👇
 // 👇 COMPLETE YOUR WORK BELOW 👇
 // 👇 COMPLETE YOUR WORK BELOW 👇
+
+/**
+ * ### Challenge `makePersonObject`
+ * 
+ * @instructions
+ * This function should take an id, a name and an email as arguments,
+ * and return an object with `id`, `name` and `email` properties.
+ * 
+ * For example, if we invoke `makePersonObject`
+ * passing 5, 'Leia' and 'leia@leia.com' as arguments,
+ * the returned value should look like:
+ * {
+ *   id: 5,
+ *   name: "Leia",
+ *   email: "leia@leia.com",
+ * }
 */
+function makePersonObject(/* code here */) {
+  /* code here */
+}
 
 /**
  * ### Challenge `getName`
+ * 
  * @instructions
- * Must return input object's `name` property.
- *
- * Sample data expected output: `Luke Skywalker`
+ * This function takes as its only argument
+ * an object containing a `name` property,
+ * and return a string that reads `Hello, my name is {name}`,
+ * where `{name}` is the name stored in the object.
+ * 
+ * For example, if we invoke `getName`
+ * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
+ * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(character) {
-  // ⭐️ Example Solution Provided For First Function ⭐️
-  return character.name
+function getName(/* code here */) {
+  /* code here */
 }
 
 /**
- * ### Challenge `getFilmCount`
+ * ### Challenge `makeSmartPerson`
+ * 
  * @instructions
- * Must return the number of elements in the `films` property.
- *
- * Sample data expected output: 5
- */
-function getFilmCount(character) {
-  // TODO: Add your code inside the functions (others below).
-
+ * This function takes a single `name` argument and returns an object.
+ * The returned object has the following characteristics:
+ *     It has a `name` property that contains the argument passed in.
+ *     It has a `sum` method that takes two numbers as arguments
+ *         and returns the result of adding them together.
+ *     It has a `speak` method that takes no arguments
+ *         and returns a string like `Hello, my name is {name}`.
+ *         where `{name}` is the name passed into `makeSmartPerson`.
+*/
+function makeSmartPerson(/* code here */) {
+  /* code here */
 }
 
+/**
+ * ### Challenge `getCarInfoByIndex`
+ * 
+ * @instructions
+ * getCarInfoByIndex takes two arguments:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ *     (2) a number which is the desired index in the array.
+ * getCarInfoByIndex returns a string in the format `This is a {car_make} {car_model}
+ * 
+ * For example, if getCarInfoByIndex is invoked with the inventory and the number 0,
+ * it will return `This is a Lincoln Navigator`.
+*/
+function getCarInfoByIndex(/* code here */) {
+  /* code here */
+}
 
+/**
+ * ### Challenge `getLastCarInfo`
+ * 
+ * @instructions
+ * getLastCarInfo takes a single argument:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ * getLastCarInfo returns a string in the format `This is a {car_make} {car_model}
+ * 
+ * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
+ * it will return `This is a Lincoln Town Car`.
+*/
+function getLastCarInfo(/* code here */) {
+  /* code here */
+}
 
+/**
+ * ### Challenge `getCarInfoById`
+ * 
+ * @instructions
+ * getCarInfoById takes two arguments:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ *     (2) a number which is the desired car id (see how each car has its own unique id).
+ * getCarInfoById returns a string in the format `This is a {car_make} {car_model}
+ * 
+ * For example, if getCarInfoById is invoked with the inventory and the number 1,
+ * it will return `This is a Lincoln Navigator`.
+*/
+function getCarInfoById(/* code here */) {
+  /* code here */
+}
 
+/**
+ * ### Challenge `sortCarInventory`
+ * 
+ * @instructions
+ * sortCarInventory takes a single argument:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
+*/
+function sortCarInventory(/* code here */) {
+  /* code here */
+}
+
+/**
+ * ### Challenge `getModelYears`
+ * 
+ * @instructions
+ * We need the years from every car in the inventory!
+ * getModelYears takes a single argument:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ * getModelYears returns an array containing all the 'car_year's in the inventory.
+*/
+function getModelYears(/* code here */) {
+  /* code here */
+}
+
+/**
+ * ### Challenge `getOlderCars`
+ * 
+ * @instructions
+ * We need a utility to find older cars!
+ * getOlderCars takes two arguments:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ *     (2) a number which is the desired max year.
+ * getOlderCars returns an array containing all the cars
+ * with a `car_year` which is at most the given desired max year,
+ * in the same order as they appear in the original inventory.
+*/
+function getOlderCars(/* code here */) {
+  /* code here */
+}
+
+/**
+ * ### Challenge `getGermanCars`
+ * 
+ * @instructions
+ * We need a utility to find German cars!
+ * getGermanCars takes a single argument:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ * getGermanCars returns an array containing all the cars
+ * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
+ * in the same order as they appear in the original inventory.
+*/
+function getGermanCars(/* code here */) {
+  /* code here */
+}
+
+/**
+ * ### Challenge refactor to arrow functions
+ * 
+ * @instructions
+ * Create arrow function versions of the following commented-out functions:
+ * 
+ * const sum = function (a, b) {
+ *   return a + b
+ * }
+ * 
+ * const addFive = function(num) {
+*    return num + 5
+ * }
+ *
+ * const argTimesTwo = function (num) {
+ *   return num * 2
+ * }
+*/
+const sum = null; // code here!
+const addFive = null; // code here!
+const argTimesTwo = null; // code here!
+
+/**
+ * ### Challenge `carMaker`
+ * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
+ * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
+ * 
+ * @instructions
+ * This function takes a single odometer argument (a number) and returns an object.
+ * The returned object has the following characteristics:
+ *     it has an `odometer` property that contains the argument passed in.
+ *     it has a `drive` method that takes a distance as its argument, and
+ *         (1) causes the odometer in the object to be increased by the distance,
+ *         (2) returns the updated value of the `odometer`.
+*/
+function carMaker(/* code here */) {
+  /* code here */
+}
 
 /// ////// END OF CHALLENGE /////////
 /// ////// END OF CHALLENGE /////////
@@ -40,8 +229,19 @@ if (typeof exports !== 'undefined') {
   // IGNORE: Test/Env Detected
   // For Node/Non-browser test env
   module.exports = module.exports || {}
+  if (firstItem) { module.exports.firstItem = firstItem }
+  if (makePersonObject) { module.exports.makePersonObject = makePersonObject }
   if (getName) { module.exports.getName = getName }
-  if (getFilmCount) { module.exports.getFilmCount = getFilmCount }
-  // TODO/INSTRUCTOR: ADD ANY FUNCTIONS YOU CREATE TO EXPORTS HERE
-  //                  USE THE LINES ABOVE AS A REFERENCE
+  if (makeSmartPerson) { module.exports.makeSmartPerson = makeSmartPerson }
+  if (carMaker) { module.exports.carMaker = carMaker }
+  if (getCarInfoByIndex) { module.exports.getCarInfoByIndex = getCarInfoByIndex }
+  if (getLastCarInfo) { module.exports.getLastCarInfo = getLastCarInfo }
+  if (getCarInfoById) { module.exports.getCarInfoById = getCarInfoById }
+  if (sortCarInventory) { module.exports.sortCarInventory = sortCarInventory }
+  if (getModelYears) { module.exports.getModelYears = getModelYears }
+  if (getOlderCars) { module.exports.getOlderCars = getOlderCars }
+  if (getGermanCars) { module.exports.getGermanCars = getGermanCars }
+  if (sum) { module.exports.sumF = sum }
+  if (addFive) { module.exports.addFiveF = addFive }
+  if (argTimesTwo) { module.exports.argTimesTwoF = argTimesTwo }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -4,32 +4,165 @@
 ////// STUDENTS: DO NOT MODIFY THIS FILE //////
 ////// STUDENTS: DO NOT MODIFY THIS FILE //////
 
-
 /* eslint-env mocha */
-if (typeof exports !== 'undefined') {
+if (typeof exports !== "undefined") {
   // IGNORE: Test/Env Detection Stuff //
   // Node/Non-browser test env
-  
-  // TODO: Add any json/data imports
-  var lukeSkywalker = require('../data/person-1.json')
-  // var leiaOrgana = require('../data/person-5.json')
-  // var obiWanKenobi = require('../data/person-10.json')
 
-  var chai = require('chai')
+  var chai = require("chai");
+
   var {
+    firstItem,
+    makePersonObject,
     getName,
-    // TODO: Add all functions to export/test here
-  } = require('../index.js')
+    makeSmartPerson,
+    carMaker,
+    getCarInfoByIndex,
+    getLastCarInfo,
+    getCarInfoById,
+    sortCarInventory,
+    getModelYears,
+    getOlderCars,
+    getGermanCars,
+    sumF,
+    addFiveF,
+    argTimesTwoF
+  } = require("../index.js");
 }
-var expect = chai.expect
+var expect = chai.expect;
 
-// TODO: Add test `describe` blocks here
+describe("firstItem()", () => {
+  it("can add two numbers", () => {
+    expect(firstItem(['bar', 'baz'], str => str + str)).to.eql('barbar');
+    expect(firstItem(['bar', 'baz'], str => str[0])).to.eql('b');
+  });
+});
 
-// Example:
-describe('getName()', () => {
-  it('expected name for `Luke`', () => { expect(getName(lukeSkywalker)).to.eq(`Luke Skywalker`) })
-  // it('expected name for `Leia`', () => { expect(getName(leiaOrgana)).to.eq(`Leia Organa`) })
-  // it('expected name for `Obi-Wan`', () => { expect(getName(obiWanKenobi)).to.eq(`Obi-Wan Kenobi`) })
-})
+// var person = { id: 1, name: "Luke", email: "luke@luke.com" };
 
+// describe("makePersonObject()", () => {
+//   it("makes an object of the right shape", () => {
+//     expect(makePersonObject(1, "Luke", "luke@luke.com")).to.eql(person);
+//   });
+// });
 
+// describe("getName()", () => {
+//   it("makes an object of the right shape", () => {
+//     expect(getName(person)).to.include("my name is Luke");
+//   });
+// });
+
+// describe("makeSmartPerson()", () => {
+//   it("makes an object with a name", () => {
+//     expect(makeSmartPerson("Sam").name).to.eql("Sam");
+//   });
+//   it("makes an object with a functioning sum method", () => {
+//     expect(makeSmartPerson("Sam").sum(1, 2)).to.eql(3);
+//   });
+//   it("makes an object with a functioning speak method", () => {
+//     expect(makeSmartPerson("Sam").speak()).to.include("Sam");
+//   });
+// });
+
+// describe("getCarInfoByIndex()", () => {
+//   it("returns a string containing the car_make", () => {
+//     expect(getCarInfoByIndex(inventory, 0)).to.include(inventory[0].car_make);
+//   });
+//   it("returns a string containing the car_model", () => {
+//     expect(getCarInfoByIndex(inventory, 0)).to.include(inventory[0].car_model);
+//   });
+// });
+
+// describe("getLastCarInfo()", () => {
+//   it("returns a string containing the car_make", () => {
+//     expect(getLastCarInfo(inventory)).to.include(
+//       inventory[inventory.length - 1].car_make
+//     );
+//   });
+//   it("returns a string containing the car_model", () => {
+//     expect(getLastCarInfo(inventory)).to.include(
+//       inventory[inventory.length - 1].car_model
+//     );
+//   });
+// });
+
+// describe("getCarInfoById()", () => {
+//   it("returns a string containing the car_make", () => {
+//     expect(getCarInfoById(inventory, 1)).to.include(inventory[0].car_make);
+//   });
+//   it("returns a string containing the car_model", () => {
+//     expect(getCarInfoById(inventory, 1)).to.include(inventory[0].car_model);
+//   });
+// });
+
+// describe("sortCarInventory()", () => {
+//   it("can sort the inventory", () => {
+//     const copyOfInventory = [...inventory];
+//     const sorted = sortCarInventory(copyOfInventory);
+//     expect(sorted[sorted.length - 1].car_model).to.eql("Yukon");
+//   });
+// });
+
+// describe("getModelYears()", () => {
+//   it("returns an array of car years", () => {
+//     const carYears = inventory.map(c => c.car_year);
+//     expect(getModelYears(inventory)).to.eql(carYears);
+//   });
+// });
+
+// describe("getOlderCars()", () => {
+//   it("returns an empty array for a very low max year", () => {
+//     const cars = inventory.filter(c => c.car_year <= 0);
+//     expect(getOlderCars(inventory, 0)).to.eql(cars);
+//   });
+//   it("returns the entire inventory for a very high max year", () => {
+//     const cars = inventory.filter(c => c.car_year <= 3000);
+//     expect(getOlderCars(inventory, 3000)).to.eql(cars);
+//   });
+//   it("returns some cars when the max year is somewhere in the middle", () => {
+//     const cars = inventory.filter(c => c.car_year <= 1990);
+//     expect(getOlderCars(inventory, 1990)).to.eql(cars);
+//   });
+// });
+
+// describe("getGermanCars()", () => {
+//   it("returns an array with the correct cars", () => {
+//     const makes = ["Audi", "Mercedes-Benz", "Volkswagen", "BMW"];
+//     const cars = inventory.filter(c => makes.includes(c.car_make));
+//     expect(getGermanCars(inventory)).to.eql(cars);
+//   });
+// });
+
+// describe("refactor into arrow functions", () => {
+//   it("sum uses arrow syntax", () => {
+//     expect(sumF.toString()).to.include("=>");
+//   });
+//   it("addFive uses arrow syntax", () => {
+//     expect(addFiveF.toString()).to.include("=>");
+//   });
+//   it("argTimesTwo uses arrow syntax", () => {
+//     expect(argTimesTwoF.toString()).to.include("=>");
+//   });
+// });
+
+// describe("carMaker() STRETCH", () => {
+//   it("makes a car with an odometer set to the correct value", () => {
+//     expect(carMaker(123).odometer).to.eql(123);
+//   });
+//   it("driving the car increases the odometer", () => {
+//     var car = carMaker(100);
+//     car.drive(1);
+//     expect(car.odometer).to.eql(101);
+//     car.drive(1);
+//     expect(car.odometer).to.eql(102);
+//   });
+//   it("driving the car returns the updated odometer", () => {
+//     expect(carMaker(100).drive(5)).to.eql(105);
+//   });
+// });
+
+////// STUDENTS: DO NOT MODIFY THIS FILE //////
+////// STUDENTS: DO NOT MODIFY THIS FILE //////
+////// STUDENTS: DO NOT MODIFY THIS FILE //////
+////// STUDENTS: DO NOT MODIFY THIS FILE //////
+////// STUDENTS: DO NOT MODIFY THIS FILE //////
