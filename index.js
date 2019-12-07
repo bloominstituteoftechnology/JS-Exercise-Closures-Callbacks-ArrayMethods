cda7dd9695d3823b52a30babe83d5e45a101a536
Bill Kramer <billykra@gmail.com>
2019-12-06 17:45:01 -0800

    JS Closures, Callbacks, ArrayMethods Q1

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(paramList, callback) {
-  /* CODE HERE */
+function processLength(list, callback) {
+  return callback (list.length);
 
 }
 
@@ -67,8 +67,8 @@ function processLength(paramList, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  return callback(stringList[-1])
 }
 
 /**
