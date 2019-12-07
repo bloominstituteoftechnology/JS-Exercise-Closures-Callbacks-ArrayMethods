030f27df8a08a1f94332cd9c4462ff8feb40ab47
Donna Ballew <ballewdd@gmail.com>
2019-12-06 23:04:04 -0800

    passed processProduct

--- index.js
+++ index.js
@@ -112,8 +112,11 @@ function processSum(numberList, callback) {
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
  */
-function processProduct(/* CODE HERE */) {
+function processProduct(num1, num2, callback) {
   /* CODE HERE */
+  let total = num1 * num2;
+
+  return callback(total);
 }
 
 /**
