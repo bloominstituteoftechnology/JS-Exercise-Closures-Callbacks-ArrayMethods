cf8e7a82be2c456104e714e5cc76a4944d258934
Laura reed <pilesoflaura@gmail.com>
2020-01-16 20:43:12 -0700

    working on MVP

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function processLength(list, callback) {
+  return callback(list.length);
+  }
 
 /**
  * ### Challenge `processLastItem`
@@ -66,8 +66,8 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  return callback(stringList[1]);
 }
 
 /**
@@ -87,9 +87,12 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  const sumNum = numberList.reduce((accum, item) => {
+    return accum + item;
+  }, 0);
 }
+console.log(sumNum);
 
 /**
  * ### Challenge `processProduct`
