2b692c71c349e228ca5653bceb201ab177b35759
Sara Reidy <sara.reidy.21@gmail.com>
2020-01-27 08:53:44 -0500

    getFullNames works as specified

--- index.js
+++ index.js
@@ -180,8 +180,8 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  return runners.map(runner => `${runner.last_name}, ${runner.first_name}`)
 }
 
 /**
