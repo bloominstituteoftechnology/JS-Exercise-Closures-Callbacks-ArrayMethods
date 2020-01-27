0db4a40673ae4dc0250d7e5a3a01230132ece83d
Sara Reidy <sara.reidy.21@gmail.com>
2020-01-27 09:17:03 -0500

    counterMakerWithLimit works as specified

--- index.js
+++ index.js
@@ -281,9 +281,19 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(maxValue) {
+  let count = -1;
+  return function counter() {
+    if(count === maxValue){
+      count = 0;
+    }
+    else{
+      ++count;
+    }
+    return count;
+  }
 }
+
 
 /////////////// END OF CHALLENGE ///////////////
 /////////////// END OF CHALLENGE ///////////////
