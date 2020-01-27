58be4fa92c3bd9e608e56efd5634d4abefb60401
Sara Reidy <sara.reidy.21@gmail.com>
2020-01-27 09:08:57 -0500

    counterMaker works as specified

--- index.js
+++ index.js
@@ -253,9 +253,10 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = -1;
+  return function counter() {
+    ++count;
+    return count;
   }
   // BROKEN CODE ENDS
 }
