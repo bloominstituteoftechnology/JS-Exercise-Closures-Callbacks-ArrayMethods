9f83922ff5587560b50ae1ea7ab1229ff02e95f2
Sara Reidy <sara.reidy.21@gmail.com>
2020-01-27 09:24:32 -0500

    processDuplicateFree works as expected

--- index.js
+++ index.js
@@ -158,8 +158,8 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  return callback([...new Set(list)]);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
