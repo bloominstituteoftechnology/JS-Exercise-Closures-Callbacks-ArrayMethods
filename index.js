345d854bd4b577d946fcb4ac4dacc4b711bdad68
ReyWilly <57641631+ReyWilly@users.noreply.github.com>
2020-01-28 17:06:44 -0500

    First challenge complete

--- index.js
+++ index.js
@@ -48,10 +48,13 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
+function processLength(list, cb) {
+  return cb(list.length)
 }
-
+const cb = (num) =>{
+  return "There are " + num;
+}
+console.log(processLength(['test','work'], cb));
 /**
  * ### Challenge `processLastItem`
  * 
@@ -66,8 +69,8 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  return (stringList)
 }
 
 /**
