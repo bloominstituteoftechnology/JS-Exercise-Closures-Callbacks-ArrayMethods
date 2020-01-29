9bb447387debbfd367b7fd653a35c1afe23a9d0b
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-01-29 09:58:44 -0600

    only 3 more problems left

--- index.js
+++ index.js
@@ -230,8 +230,10 @@ function firstNamesAllCaps(array) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(array, tShirtSize) {
+  const runnerSize = array.filter(function(item){
+    return tShirtSize.indexOf(array.shirt_size) > -1; 
+  })
 }
 
 /**
