ceba5a71f470ef28c4f6f78abe47d7a4a72d8326
thayne-allen <59717714+thayne-allen@users.noreply.github.com>
2020-02-14 19:42:43 -0700

    processLastItem

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE! */) {
-  /* CODE HERE! */
+function processLength(list, callback) {
+  return callback(list)
 }
 
 /**
@@ -66,8 +66,8 @@ function processLength(/* CODE HERE! */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  return callback(stringList[1])
 }
 
 /**
@@ -87,8 +87,8 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  return callback(numberList)
 }
 
 /**
