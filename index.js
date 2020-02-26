816d182eb00212bf7dc1ee1e48e6a50601525947
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-02-25 20:25:47 -0800

    mvp met, stretch not attempted

--- index.js
+++ index.js
@@ -264,9 +264,6 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
   let count = 0;
   return function() {
     ++count;
@@ -276,7 +273,7 @@ function counterMaker() {
   }
 
   // BROKEN CODE ENDS
-}
+
 
 /**
  * ### Challenge `counterMakerWithLimit`
