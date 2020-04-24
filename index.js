e55e7a7954d7d6ace9d3c84395587dbad4e1af51
Aaron Yates <aaronmyates@hotmail.com>
2020-04-24 18:53:37 -0400

    problem 1 done

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
+function processLength(list, callback) {
   /* CODE HERE */
+  return callback(list.length);
 }
 
 /**
