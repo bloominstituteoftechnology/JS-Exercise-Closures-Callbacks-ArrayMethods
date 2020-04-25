97449d6bf76ab566c27be2bb68a3f88fb7dd86f0
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-04-24 21:11:35 -0600

    processDuplicateFree() stretch question complete

--- index.js
+++ index.js
@@ -132,8 +132,8 @@ function processProduct(num1, num2, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  return callback(list.filter((listItem, index) => list.indexOf(listItem) === index));
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
