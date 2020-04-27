957b31a6e3ca672412efa3fc95c056cd1884d15e
JenVest2020 <jenvest2019@gmail.com>
2020-04-27 13:25:55 -0500

    passed processDuplicateFree function twice

--- index.js
+++ index.js
@@ -132,8 +132,9 @@ function processProduct(num1, num2, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  let DupeFreeSet = new Set(list);
+  return callback(Array.from(DupeFreeSet));
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
